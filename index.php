bebd5f3a247132b0a41b943e4e541b1938489dad
Sara Battilotti <sara.battilotti@gmail.com>
2023-08-27 14:04:37 +0200

    styling added

--- index.php
+++ index.php
@@ -41,9 +41,21 @@
     </script>
 </head>
 <body>
+<div id="bubbles"></div>
+<div id="shark">
+  <div class="shark-body"></div>
+  <div class="shark-eye"></div>
+  <div class="aleta"></div>
+  <div class="tail"></div>
+  <div class="fin"></div>
+  <div class="gill gill-1"></div>
+  <div class="gill gill-2"></div>
+  <div class="gill gill-3"></div>
+</div>
   <div class="container">
+    <h1> Shark Film Wiki </h1>
     <form>
-     <input data-search type="text" placeholder="type here" />
+     <input id="input" data-search type="text" placeholder="trova film" />
     </form>
     <ul id="movieList">
       <li> </li>
--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 if (isset($_POST['searchVal'])) {
-    $query = "SELECT * FROM filmsquali WHERE titolo LIKE '%{$_POST['searchVal']}%' LIMIT 5";
+    $query = "SELECT * FROM filmsquali WHERE titolo LIKE '%{$_POST['searchVal']}%' LIMIT 10";
     $result = mysqli_query($mysqli, $query);
   if (mysqli_num_rows($result) > 0) {
      $rows = mysqli_fetch_all($result, MYSQLI_ASSOC); // Fetch rows as an associative array
